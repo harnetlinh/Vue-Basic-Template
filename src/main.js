d50363cf61d9726faad403f57412b24637313a67
linh.hngoc <linh.hngoc@anlab.pro>
2020-06-23 14:03:55 +0700

    add bootstrap css

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import VueRouter from 'vue-router'
 import FlagIcon from 'vue-flag-icon';
 import i18n from '@/plugins/i18n';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(FlagIcon);
 Vue.use(VueRouter)
